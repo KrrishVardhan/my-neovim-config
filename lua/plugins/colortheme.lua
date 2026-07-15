952db58d73c192b22e1d00d1ad3ee3f8813a0692
KrrishVardhan <vyaskrrishvardhan@gmail.com>
2026-07-16 02:00:36 +0530

    yeah tranparent is BAD

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,30 +1,8 @@
-local transparent = false
-
-local function load_theme()
-  require("tokyonight").setup({
-    style = "night",
-    transparent = transparent,
-
-    styles = {
-      sidebars = transparent and "transparent" or "dark",
-      floats = transparent and "transparent" or "dark",
-    },
-  })
-
-  vim.cmd.colorscheme("tokyonight")
-end
-
-return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-
-  config = function()
-    load_theme()
-
-    vim.keymap.set("n", "<leader>bg", function()
-      transparent = not transparent
-      load_theme()
-    end, { desc = "Toggle Background Transparency" })
-  end,
+return { 
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function() 
+        vim.cmd.colorscheme("tokyonight-night") 
+    end, 
 }

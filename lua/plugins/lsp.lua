db3326dd19abb24a36269998e75c9622fa3fc1d4
KrrishVardhan <vyaskrrishvardhan@gmail.com>
2026-07-17 23:01:50 +0530

    added emmet for html and jsx tsx

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -17,11 +17,38 @@ return {
           ensure_installed = {
             "ts_ls",
             "html",
+            "emmet_language_server",
           },
           automatic_enable = true,
         },
       },
     },
+    config = function()
+        vim.lsp.config("emmet_language_server", {
+            filetypes = {
+                "html",
+                "css",
+                "javascriptreact",
+                "typescriptreact",
+            },
+        })
+
+        vim.lsp.enable("emmet_language_server")
+
+        vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(event)
+                local opts = { buffer = event.buf }
+
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+            end,
+        })
+    end,
   },
 
   {

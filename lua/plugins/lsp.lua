b4309256e6c55606f2d693ae9388f1d4d4bc5b39
KrrishVardhan <vyaskrrishvardhan@gmail.com>
2026-07-21 22:45:35 +0530

    added intelephense and harpoon list clear

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,6 +18,7 @@ return {
             "ts_ls",
             "html",
             "emmet_language_server",
+            "intelephense",
             -- "jdtls",
           },
           automatic_enable = true,

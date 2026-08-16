2c0166f71dd29b7bc199415c08bed43a652efdec
KrrishVardhan <vyaskrrishvardhan@gmail.com>
2026-08-17 03:43:25 +0530

    Enhance colortheme configuration with transparency
    Added support for transparent background in specified highlight groups.

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,8 +1,24 @@
-return { 
+return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    config = function() 
-        vim.cmd.colorscheme("tokyonight-night") 
-    end, 
+    config = function()
+        vim.cmd.colorscheme("tokyonight-night")
+
+        local transparent_groups = {
+            "Normal",
+            "NormalNC",
+            "NormalFloat",
+            "FloatBorder",
+            "SignColumn",
+            "EndOfBuffer",
+            "MsgArea",
+            "NvimTreeNormal",
+            "NvimTreeNormalNC",
+        }
+
+        for _, group in ipairs(transparent_groups) do
+            vim.api.nvim_set_hl(0, group, { bg = "none" })
+        end
+    end,
 }
